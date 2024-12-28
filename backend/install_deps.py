a3caba5155bd8ac734f4091d869e5e9659df9bfe
Aman Ibrahim <amanmibra@gmail.com>
2024-12-28 08:50:08 -0700

    Add install time log

--- backend/install_deps.py
+++ backend/install_deps.py
@@ -1,11 +1,18 @@
 """For docker container only, installs models"""
 
+import time
+
 import torch
 
 # Run at buildtime
+start_time = time.time()
+
 torch.hub.load(
     repo_or_dir="snakers4/silero-vad",
     model="silero_vad",
     force_reload=True,
     trust_repo=True,
 )
+
+end_time = time.time()
+print(f"Silero VAD installation took {end_time - start_time:.2f} seconds")
